d193b2ce2f1b3903e6ff92a0a077fa6c2f441b52
Sipos <jsipo@uic.edu>
2026-03-18 15:36:11 -0500

    init commit

---
+++ lab5/Debug/Sources/main.d
@@ -0,0 +1,100 @@
+Sources/main.o: ../Sources/main.c \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/Cpu.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Types.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Error.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Const.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/IO_Map.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/IO_Map/MKL25Z4.h \
+ ../Sources/Events.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Types.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Error.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Const.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/IO_Map.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PWM1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PwmLdd1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/TU1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/PDD/TPM_PDD.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/PDD/PDD_Types.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/Cpu.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PwmLdd1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/TU1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PWM2.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PwmLdd2.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/TU2.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PwmLdd2.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/TU2.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/Bit1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/PDD/GPIO_PDD.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/AS1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/ASerialLdd1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/ASerialLdd1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/AD1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/AdcLdd1.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/PDD/ADC_PDD.h \
+ C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/AdcLdd1.h
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/Cpu.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Types.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Error.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Const.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/IO_Map.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/IO_Map/MKL25Z4.h:
+
+../Sources/Events.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Types.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Error.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PE_Const.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/IO_Map.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PWM1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PwmLdd1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/TU1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/PDD/TPM_PDD.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/PDD/PDD_Types.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/Cpu.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PwmLdd1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/TU1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PWM2.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PwmLdd2.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/TU2.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/PwmLdd2.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/TU2.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/Bit1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/PDD/GPIO_PDD.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/AS1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/ASerialLdd1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/ASerialLdd1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/AD1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/AdcLdd1.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Static_Code/PDD/ADC_PDD.h:
+
+C:/Users/jsipo/Documents/lab5a/lab5/Generated_Code/AdcLdd1.h:
